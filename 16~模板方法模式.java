176c01927c9adc48610333fb15227cb167fc752c
JimmyYao1984 <jimmyyao1984@gmail.com>
2019-03-06 18:03:14 +0800

    Create 16~模板方法模式.java

---
+++ 16~模板方法模式.java
@@ -0,0 +1,38 @@
+//模板方法模式
+public abstract class ABike{
+	protected final void makeBike(){
+		this.makeBody();
+		this.makeSeat();
+		this.makeBrake();
+		this.makeWheel();
+		this.makeColor();
+	}
+
+	final void makeBody(){
+		System.out.println("制造车架");
+	}
+
+	final void makeSeat(){
+		System.out.println("制造凳子");
+	}
+
+	final void makeBrake(){
+		System.out.println("制造刹车系统");
+	}
+
+	abstract void makeColor();
+}
+
+public class Mobike extends ABike{
+	@Override
+	void makeColor(){
+		System.out.println("喷上迷人的红色");
+	}
+}
+
+public class OfoBike extends ABike{
+	@Override
+	void makeColor(){
+		System.out.println("喷骚气的黄色");
+	}
+}
