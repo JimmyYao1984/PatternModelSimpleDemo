b1c13f2da5f1e97f1f79b247ec42f3947cae22f4
JimmyYao1984 <jimmyyao1984@gmail.com>
2019-03-21 16:21:33 +0800

    Update 8-代理模式.java

--- 8-代理模式.java
+++ 8-代理模式.java
@@ -1,41 +1,41 @@
-//代理模式 访问控制
-public class Account{
+//代理模式，访问控制
+public class Account {
+
 }
 
-public interface Driveable{
-	void drive();
+public interface Drivable {
+    void drive();
 }
 
 //没有账户不让骑
-public Bike implements Driveable{
-	public Account account;
+public class Bike implements Drivable {
+    public Account account;
 
-	@Override
-	public void drive(){
-		if(account == null){
-			return;
-		}
-		System.out.println("骑单车")
-	}
+    @Override
+    public void drive() {
+        if (account == null) {
+            return;
+        }
+        System.out.println("骑单车");
+    }
 }
 
 //代理有账户
-public Proxy implement Driveable{
-	private Bike bike;
+public class Proxy implements Drivable {
+    private Bike bike;
 
-	public Proxy(Bike bike){
-		this.bike = bike;
-	}
+    public Proxy(Bike bike){
+        this.bike = bike;
+    }
 
-	@Override
-	public void drive(){
-		bike.account  = new Account();
-		bike.drive();
-	}
-
+    @Override
+    public void drive(){
+        bike.account = new Account();
+        bike.drive();
+    }
 }
 
 public static void main(String[] args){
-	Driveable driveable = new Proxy(new Bike());
-	driveable.drive();
+    Drivable drivable = new Proxy(new Bike());
+    drivable.drive();
 }
