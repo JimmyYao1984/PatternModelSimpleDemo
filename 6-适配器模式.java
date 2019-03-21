5e741480fd02e6c2211979c165fc35f759836ff1
JimmyYao1984 <jimmyyao1984@gmail.com>
2019-03-21 15:55:55 +0800

    Update 6-适配器模式.java

--- 6-适配器模式.java
+++ 6-适配器模式.java
@@ -1,30 +1,29 @@
 //适配器模式
 public abstract class Bike{
-	public void drive();
+    public void drive();
 }
 
 public class OfoBike extends Bike{
-	public void drive(){
-		System.out.println("骑小黄车")；
-	}
+    public void drive(){
+        System.out.println("骑小黄车");
+    }
 }
-
 
 //扫码骑摩拜
-public interface ScanMobi{
-	void scanMobi();
+public interface  ScanMobi{
+    void scanMobi(Bike bike);
 }
 
-//黑客通过摩拜APP扫码骑其他公司的车
+//黑客通过摩拜app扫码骑其他公司的车
 public class HackerAdapter implements ScanMobi{
-	void scanMobi(Bike bike){
-		bike.drive();
-	}
+    void scanMobi(Bike bike){
+        bike.drive();
+    }
 }
 
 public static void main(String[] args){
-	HackerAdapter hacker = new HackerAdapter();
-	OfoBike bike = new OfoBike();
-	//只有摩拜app的扫码接口，照样可以骑小黄车
-	hacker.scanMobi(bike);
+    HackerAdapter hackerAdapter = new HackerAdapter();
+    OfoBike bike = new OfoBike();
+    //只有摩拜app，照样可以骑小黄车
+    hackerAdapter.scanMobi(bike);
 }
