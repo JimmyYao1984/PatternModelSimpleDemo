44f1499760a5295f5c7671cb5d8219e882611e61
JimmyYao1984 <jimmyyao1984@gmail.com>
2019-03-21 17:02:26 +0800

    Update 11-组合模式.java

--- 11-组合模式.java
+++ 11-组合模式.java
@@ -1,81 +1,81 @@
 //组合模式 部分-整体 树形结构
-public abstract class BikeStuff{
-	public String name;
-	public String region;
+public abstract class BikeStuff {
+    public String name;
+    public String region;
 
-	public void add(BikeStuff stuff){
-		throw new UnsupportedOperationException("不支持添加操作");
-	}
+    public void add(BikeStuff stuff) {
+        throw new UnsupportedOperationException("不支持添加操作");
+    }
 
-	public void remove(BikeStuff stuff){
-        	throw new UnsupportedOperationException("不支持删除操作");
-    	}
+    public void remove(BikeStuff stuff) {
+        throw new UnsupportedOperationException("不支持删除操作");
+    }
 
-	public void report(){
-        	throw new UnsupportedOperationException("不支持汇报操作");
-    	}
+    public void report() {
+        throw new UnsupportedOperationException("不支持汇报操作");
+    }
 }
 
 //修理人员
-public class BikeRepairer extends BikeStuff{
-	public BikeRepairer(String name,String region){
-		this.name = name;
-		this.region = region;
-	}
+public class BikeRepairer extends BikeStuff {
+    public BikeRepairer(String name, String region) {
+        this.name = name;
+        this.region = region;
+    }
 
-	@Override
-	public void report(){
-		System.out.println("负责" + region + "的修理运营");
-	}
+    @Override
+    public void report() {
+        System.out.println("负责" + region + "的修理运营");
+    }
 }
 
 //运营经理
-public class BikeManager extends BikeStuff{
-	private List<BikeStuff> members = new ArrayList<BikeStuff>();
-	public Integer level;
+public class BikeManager extends BikeStuff {
+    private List<BikeStuff> members = new ArrayList<BikeStuff>();
+    public Integer level;
 
-	public BikeManager(String name,String region,Integer level){
-		this.name = name;
-		this.region = region;
-	}
+    public BikeManager(String name, String region, Integer level) {
+        this.name = name;
+        this.region = region;
+    }
 
-	public void add(BikeStuff stuff){
-		members.add(stuff);	
-	}
+    public void add(BikeStuff stuff) {
+        members.add(stuff);
+    }
 
-	public void remove(BikeStuff stuff){
-       		members.remove(stuff);
-    	}
+    public void remove(BikeStuff stuff) {
+        members.remove(stuff);
+    }
 
-	public void report(){
-		System.out.println(this.name + "负责" + this.region + "的管理和运营");
-        	for(BikeStuff stuff : members){
-            		if(this.level != null){
-                	for(int  i = 0; i < this.level; i++){
-                    		System.out.print("  ");
-                	}
-            	}
-            	stuff.report();
+    public void report() {
+        System.out.println(this.name + "负责" + this.region + "的管理和运营");
+        for (BikeStuff stuff : members) {
+            if (this.level != null) {
+                for (int i = 0; i < this.level; i++) {
+                    System.out.println(" ");
+                }
+            }
+            stuff.report();
         }
     }
 }
 
-public static void main(String[] args){
-	BikeStuff xiaoZhang = new BikeRepairer("xiaoZhang","徐泾");
-	BikeStuff xiaoZhao = new BikeRepairer("xiaoZhao","赵巷");
-	BikeStuff laoWang = new BikeManager("laoWang","青浦",2);
-	laoWang.add(xiaoZhao);
-	laoWang.add(xiaoZhang);
+    public static void main(String[] args) {
+        BikeStuff xiaozhang = new BikeRepairer("xiaozhang", "徐泾");
+        BikeStuff xiaozhao = new BikeRepairer("xiaozhao", "赵巷");
+        BikeStuff laoWang = new BikeManager("laowang", "青浦", 2);
+        laoWang.add(xiaozhao);
+        laoWang.add(xiaozhang);
 
-	BikeStuff xiaoLi = new BikeRepairer("xiaoLi","北蔡");
-	BikeStuff xiaoTao = new BikeRepairer("xiaoZhao","塘桥");
-	BikeStuff laoZheng = new BikeManager("laoWang","浦东",2);
-	laoZheng.add(xiaoLi);
-	laoZheng.add(xiaoTao);
+        BikeStuff xiaoli = new BikeRepairer("xiaoli", "北蔡");
+        BikeStuff xiaotao = new BikeRepairer("xiaotao", "塘桥");
+        BikeStuff laozheng = new BikeManager("laozheng", "浦东", 2);
+        laozheng.add(xiaoli);
+        laozheng.add(xiaotao);
 
-	BikeStuff laoXu = new BikeManager("laoXu","上海",1);
-	laoXu.add(laoWang);
-	laoXu.add(laoZheng);
+        BikeStuff laoxu = new BikeManager("laoxu", "上海", 1);
+        laoxu.add(laoWang);
+        laoxu.add(laozheng);
 
-	laoXu.report();
-}
+        laoxu.report();
+    }
